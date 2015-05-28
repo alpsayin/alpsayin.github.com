bdfd787dc03b6f611dc28f13f302c5cbd5e8d5ef
Alp Sayin <alpsayin@gmail.com>
2015-05-28 02:06:31 +0100

    bugfixes for whoishome script

--- whoIsHome.sh
+++ whoIsHome.sh
@@ -9,7 +9,16 @@ echo `date`
 clients=`iw dev $deviceName station dump | awk '{ if($1 == "Station" ) print $2 }'`
 for client in $clients
 do
-	echo $client
-	awk -v client=$client '{ if($1=="option" && $2=="mac") { if($3=="\x27"client"\x27" ) { print "Static Dhcp Hostname \t->\t " lastName } }; if($1=="option" && $2=="name") lastName=$3 }' $dhcpReservationFile
-	awk -v client=$client '{ if($2==client) { print $3 " \t\t->\t " "\x27"$4"\x27" } }' $dhcpLeasesFile
+	# staticHostname=`awk -v client=$client '{ if($1=="option" && $2=="mac") { if($3=="\x27"client"\x27" ) { print lastName } }; if($1=="option" && $2=="name") lastName=$3 }' $dhcpReservationFile`
+	leaseIp=`awk -v client=$client '{ if($2==client) { print $3 } }' $dhcpLeasesFile`
+	leaseHostname=`awk -v client=$client '{ if($2==client) { print "\x27"$4"\x27" } }' $dhcpLeasesFile`
+	
+	echo -e "Mac Address\t->\t$client"
+	echo -e "Ip Address\t->\t$leaseIp"
+	if [[ "$staticHostname" != "$leaseHostname" && -z "staticHostname" ]]
+	then
+		echo -e "Static Hostname\t->\t$staticHostname \nLease Hostname\t->\t$leaseHostname \n";
+	else
+		echo -e "Hostname\t->\t$leaseHostname \n"
+	fi
 done
